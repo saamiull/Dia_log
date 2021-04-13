485023ec84abeaaa1cb789521de2d1c584500588
sami2905 <bsef18a028@pucit.edu.pk>
2021-04-14 00:02:13 +0500

    BSEF18A028_Sami-Ul-Haq

---
+++ Dialog/app/src/main/java/org/samiulhaq/dialog/MainActivity.java
@@ -0,0 +1,57 @@
+package org.samiulhaq.dialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnDialog);
+    }
+
+    public void ShowDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Dialog Title");
+        builder.setMessage("It is message of my Dialog");
+
+        builder.setCancelable(false);
+        LayoutInflater inflater = this.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.my_layout_dialog,null));
+
+        builder.setPositiveButton("Positive",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Positive Button was Clicked", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        builder.setNegativeButton("Negative",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Negative Button was Clicked", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+
+        );
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+}
